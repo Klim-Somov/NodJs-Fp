cd24ffd8226410999d148da7d6c6ccb94ba1ac64
Klim <klim_somov@mail.ru>
2022-05-22 13:24:43 +0300

    npm i colors

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import colors from "colors";
 // console.log(process.argv);
 
 // const [name1, name2] = process.argv.slice(2);
 // console.log(`Hello to ${name1} end ${ name2}`);
+
+console.log(colors.red('Hello to Klim' ))
