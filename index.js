e15ad03db4938b553ef28831de71790418dc5212
Klim <klim_somov@mail.ru>
2022-05-22 13:16:26 +0300

    Подключение es6 мудулей. для импорта через синтаксис еS6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(process.argv);
+// console.log(process.argv);
 
-const [name1, name2] = process.argv.slice(2);
-console.log(`Hello to ${name1} end ${ name2}`);
+// const [name1, name2] = process.argv.slice(2);
+// console.log(`Hello to ${name1} end ${ name2}`);
