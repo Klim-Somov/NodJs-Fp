8399f01a15d02e6481626c1c0eacff4322aadda3
Klim <klim_somov@mail.ru>
2022-05-22 13:36:42 +0300

    команда npm ci  для разворачивания проекта изи pacsje-loc.json. с фиксированными версиями завасимостей

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import colors from "colors";
 // const [name1, name2] = process.argv.slice(2);
 // console.log(`Hello to ${name1} end ${ name2}`);
 
-console.log(colors.red('Hello to Klim' ))
+console.log(colors.red('Hello to Klim'))
